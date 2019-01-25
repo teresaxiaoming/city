0ddd5d077b156a6132acd87dd047920df907104f
teresaXDX <1094930121@qq.com>
2019-01-25 11:47:13 +0800

    a

---
+++ dist/precache-manifest.6ab65fc5dc6a669801938cb8be967b45.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4bf2c403cb0d0fe78fec5a97f6d1f9d",
+    "url": "/fonts/iconfont.f4bf2c40.eot"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "5f9c1804007ce6609ae7",
+    "url": "/js/chunk-vendors.236e3fe9.js"
+  },
+  {
+    "revision": "82a85e47cb85f40514f7",
+    "url": "/js/app.092226f8.js"
+  },
+  {
+    "revision": "5a0937e8d20f065e3e3e307c8452710c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfb3c5510a597e321666cc2c88cf07db",
+    "url": "/img/yundong.cfb3c551.gif"
+  },
+  {
+    "revision": "32863308977aa5f61ddbac58325d993f",
+    "url": "/img/iconfont.32863308.svg"
+  },
+  {
+    "revision": "ee758fdde1de7e7fc1004b6aa917df0d",
+    "url": "/img/detail.ee758fdd.png"
+  },
+  {
+    "revision": "da62db8ffc72bad9355405ef5a953b0e",
+    "url": "/fonts/iconfont.da62db8f.ttf"
+  },
+  {
+    "revision": "1e9b18079d7e5577ccc6b433b2d4c4c7",
+    "url": "/fonts/iconfont.1e9b1807.woff"
+  },
+  {
+    "revision": "5f9c1804007ce6609ae7",
+    "url": "/css/chunk-vendors.b32848a0.css"
+  },
+  {
+    "revision": "82a85e47cb85f40514f7",
+    "url": "/css/app.ff4a461c.css"
+  }
+];
